18717d5935581673029b423f7ca230f9103fae85
Soham Mitra <soham.business777@gmail.com>
2026-01-09 22:14:54 +0530

    cross-origin adjusted for production

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./modules/routes.exports";
 import express, { Request, Response } from "express";
+import env from "./core/config/env";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+if (env.NODE_ENV === "development") {
+    app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+}
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
